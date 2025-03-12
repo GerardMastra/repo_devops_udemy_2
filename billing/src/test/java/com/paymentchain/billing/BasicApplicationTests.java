bcdd135ca8764c2229a307cf6d4f3a170a59f38b
Gerard <geramastra@gmail.com>
2025-03-12 06:52:09 -0300

    add test modification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -10,7 +10,7 @@ public class BasicApplicationTests  {
 
     @Test
     void contextLoads() {
-       String message = "Default Message Cambio test devops 11_";
+       String message = "Default Message Cambio 11";
         assertNotNull(message);
     }
 }
